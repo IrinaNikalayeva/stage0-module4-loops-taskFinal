5d8b5ed6e15beb6830107c4d38103e57455f75fe
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-11 21:54:50 +0100

    Performed Power task

--- src/main/java/school/mjc/stage0/loops/finalTask/Power.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Power.java
@@ -2,10 +2,15 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Power {
     public void printPower(int numberToPrint, int power) {
+        int finalNum = 1;
+        for (int i = 1; i <= power; i++) {
+            finalNum *= numberToPrint;
+        }
+        System.out.println(finalNum);
 
     }
 
     public static void main(String[] args) {
-        new Power().printPower(10, 3);
+        new Power().printPower(10, 4);
     }
 }
