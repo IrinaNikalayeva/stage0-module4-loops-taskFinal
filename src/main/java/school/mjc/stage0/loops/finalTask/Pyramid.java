4e39881b611763a1fdd2605af09dd73bcc511a2c
Iryna_Nikalayeva <irinnik92@gmail.com>
2023-01-11 21:47:16 +0100

    Performed Pyramid task

--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,7 +2,18 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        for (int i = 1; i <= cathetusLength; i++) {
 
+            for (int j = i; j < cathetusLength; j++) {
+                System.out.print(' ');
+            }
+
+            for (int j = 1; j <= i * 2 - 1; j++) {
+                System.out.print(Math.abs(i - j) + 1);
+            }
+
+            System.out.println();
+        }
     }
 
     public static void main(String[] args) {
